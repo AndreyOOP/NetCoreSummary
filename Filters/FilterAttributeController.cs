3990691fbd4f74d3d129a0771d44c7c40ec554e2
AndreyOOP <andrew_yudaichev@i.ua>
2021-08-24 00:28:56 +0300

    Add FilterAttribute sample

---
+++ Filters/FilterAttributeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filters
+{
+    [Route("filter")]
+    public class FilterAttributeController : ControllerBase
+    {
+        [HttpGet("attribute")]
+        // at the response headers will be added TestHeader = Hello!
+        [ResultFilterSample("TestHeader", "Hello!")] 
+        public IActionResult TestFilterAttribute()
+        {
+
+            return Ok();
+        }
+    }
+
+    public class ResultFilterSampleAttribute : ResultFilterAttribute
+    {
+        private string name;
+        private string value;
+
+        public ResultFilterSampleAttribute(string name, string value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add(name, new string[] { value });
+            base.OnResultExecuting(context);
+        }
+    }
+}
